4b47067ab91857ad5272e0f38cdf3fd831b30fb6
Jared Sierra <sierrajared.h@gmail.com>
2022-11-28 09:38:03 -0600

    Cors

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import express from 'express'
+import cors from 'cors'
 import indexRoutes from './routes/index.routes.js'
 import productosRoutes from './routes/productos.routes.js'
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 app.use(indexRoutes)
 app.use('/api/', productosRoutes)
