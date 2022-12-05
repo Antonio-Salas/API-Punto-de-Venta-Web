c6abc83b2499a472d8180c17b77ee68b3095912e
Jared Sierra <sierrajared.h@gmail.com>
2022-12-05 09:05:40 -0600

    Commit

--- src/controllers/login.controller.js
+++ src/controllers/login.controller.js
@@ -8,14 +8,15 @@ export const postLogin = async (req, res) => {
 
     try {
         if(email && password){
-     const [user] = await pool.query('SELECT * FROM usuario WHERE email = ?',[email], async (error,result) =>{
-        if(result.length == 0 || !(await compare(password,result[0].password))){
-            res.send('Usuario y/o password incorrectas');
-        }else{
-            res.send('Login Correcto')
-        }
+     const [user] = await pool.query('SELECT * FROM usuario WHERE email = ?',[email]) 
+    // async (error,result) =>{
+    //     if(result.length == 0 || !(await compare(password,result[0].password))){
+    //         res.send('Usuario y/o password incorrectas');
+    //     }else{
+    //         res.send('Login Correcto')
+    //     }
         
-     }) 
+    //  }) 
      res.send(user)
         }else{
             res.send('Usuario y/o contraseña no introducidas')
